b21fc13763172de82c7a538b55d88f63b70e59c3
youxuoq <17770522165@163.com>
2019-07-09 17:50:34 +0800

    首页结构搭建完成

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//引入样式
-
 //不提示开发环境
 Vue.config.productionTip = false
 
+//引入样式
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 
 new Vue({
   //渲染 导入的app模块
